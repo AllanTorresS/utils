6233d2a0b3b57601694b69163421c3de5b3a53ba
Lucas Rodrigues Teixeira Nunes <lucas.nunes@radixeng.com.br>
2020-07-07 16:54:08 -0300

    Adicionando novos arquivos

---
+++ src/main/java/ipp/aci/boleia/dominio/vo/RespostaIntegracaoVo.java
@@ -0,0 +1,32 @@
+package ipp.aci.boleia.dominio.vo;
+
+/**
+ * Resposta da tentativa de autenticação entre serviços do Pró-Frotas
+ */
+public class RespostaIntegracaoVo {
+
+    private Integer statusResposta;
+    private String tokenJwt;
+
+    /**
+     * Construtor padrão
+     */
+    public RespostaIntegracaoVo() {
+    }
+
+    public Integer getStatusResposta() {
+        return statusResposta;
+    }
+
+    public void setStatusResposta(Integer statusResposta) {
+        this.statusResposta = statusResposta;
+    }
+
+    public String getTokenJwt() {
+        return tokenJwt;
+    }
+
+    public void setTokenJwt(String tokenJwt) {
+        this.tokenJwt = tokenJwt;
+    }
+}
