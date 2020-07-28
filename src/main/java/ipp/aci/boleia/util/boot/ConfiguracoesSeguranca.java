fe0ab3f518f185f0c614a1bac6ea84aa9c1a9d00
Laranjeiro <rafael.laranjeiro@radixeng.com.br>
2020-07-28 00:30:06 -0300

    Unused Imports - release 4.0.7

--- src/main/java/ipp/aci/boleia/util/boot/ConfiguracoesSeguranca.java
+++ src/main/java/ipp/aci/boleia/util/boot/ConfiguracoesSeguranca.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
