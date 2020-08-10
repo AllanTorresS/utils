d16403aac109d833ca942c58db08e0ab925a09ab
rafael.cunha <rafael.cunha@radixeng.com.br>
2020-08-10 11:04:16 -0300

    Fix Merge, #55501

--- src/main/java/ipp/aci/boleia/util/rotas/Rotas.java
+++ src/main/java/ipp/aci/boleia/util/rotas/Rotas.java
@@ -75,6 +75,7 @@ public final  class Rotas {
     public static final  String PRODUTO_API                          = BASE_API + "/produto";
     public static final  String AGENCIADOR_FRETE_API                 = BASE_API + "/agenciadorfrete";
     public static final  String MOTORISTA_AUTONOMO_API               = BASE_API + "/motoristaautonomo";
+    public static final  String AGENCIADOR_FRETE_TAXA_API            = BASE_API + "/agenciadorfrete/taxa";
     public static final  String NOTA_FISCAL_API                      = BASE_API + "/notaFiscal";
     public static final  String TRANS_CONSOL_API                     = BASE_API + "/transacaoConsolidada";
     public static final  String TRANS_CONSOL_FROTA_API               = BASE_API + "/transacaoConsolidadaFrota";
