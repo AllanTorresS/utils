b884cd2c1b43290a72073ee4c904b6f679e98ed8
fernando.bittencourt <fernando.bittencourt@radixeng.com.br>
2020-08-19 09:57:15 -0300

    Backend do relatório de cobrança para agenciador de frete, #56343

--- src/main/java/ipp/aci/boleia/dados/oracle/OracleCobrancaAgenciadorFreteDados.java
+++ src/main/java/ipp/aci/boleia/dados/oracle/OracleCobrancaAgenciadorFreteDados.java
@@ -1,15 +1,20 @@
 package ipp.aci.boleia.dados.oracle;
 
 import ipp.aci.boleia.dados.ICobrancaAgenciadorFreteDados;
-import ipp.aci.boleia.dominio.SistemaExterno;
-import ipp.aci.boleia.dominio.agenciadorfrete.AgenciadorFrete;
 import ipp.aci.boleia.dominio.agenciadorfrete.Cobranca;
-import ipp.aci.boleia.dominio.pesquisa.parametro.ParametroPesquisaIgual;
+import ipp.aci.boleia.dominio.pesquisa.comum.ParametroPesquisa;
+import ipp.aci.boleia.dominio.pesquisa.comum.ResultadoPaginado;
+import ipp.aci.boleia.dominio.pesquisa.parametro.ParametroPesquisaDataMaiorOuIgual;
+import ipp.aci.boleia.dominio.pesquisa.parametro.ParametroPesquisaDataMenorOuIgual;
+import ipp.aci.boleia.dominio.vo.agenciadorfrete.FiltroRelatorioCobrancaVo;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
- * Respositorio de entidades Agenciador de Frete
+ * Respositorio de entidades de Cobrança de Agenciador de Frete
  */
 @Repository
 public class OracleCobrancaAgenciadorFreteDados extends OracleRepositorioBoleiaDados<Cobranca> implements ICobrancaAgenciadorFreteDados {
@@ -20,5 +25,17 @@ public class OracleCobrancaAgenciadorFreteDados extends OracleRepositorioBoleiaD
      */
     public OracleCobrancaAgenciadorFreteDados() {
         super(Cobranca.class);
+    }
+
+    @Override
+    public ResultadoPaginado<Cobranca> pesquisar(FiltroRelatorioCobrancaVo filtro) {
+        List<ParametroPesquisa> parametros = new ArrayList<>();
+        if (filtro.getAte() != null) {
+            parametros.add(new ParametroPesquisaDataMenorOuIgual("dataCriacao", filtro.getAte()));
+        }
+        if (filtro.getDe()!= null) {
+            parametros.add(new ParametroPesquisaDataMaiorOuIgual("dataCriacao", filtro.getDe()));
+        }
+        return pesquisar(filtro.getPaginacao(), parametros.toArray(new ParametroPesquisa[parametros.size()]));
     }
 }
