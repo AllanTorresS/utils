0c245a316991b9131f4edb435841c7ee9d5d8b17
rafael.cunha <rafael.cunha@radixeng.com.br>
2020-09-02 17:42:56 -0300

    Consultar Reembolso (PV) de lançamentos de Motorista Autônomos, #AZR-58789

---
+++ src/main/java/ipp/aci/boleia/dominio/vo/FiltroPesquisaReembolsoAgenciadorFreteVo.java
@@ -0,0 +1,40 @@
+package ipp.aci.boleia.dominio.vo;
+
+import ipp.aci.boleia.dominio.pesquisa.comum.BaseFiltroPaginado;
+
+/**
+ * Filtro para pesquisa de unidades e empresas agregadas.
+ */
+public class FiltroPesquisaReembolsoAgenciadorFreteVo extends BaseFiltroPaginado {
+    private String de;
+    private String ate;
+    private EnumVo tipoEntidade;
+
+    public FiltroPesquisaReembolsoAgenciadorFreteVo() {
+        //Construtor default
+    }
+
+    public String getDe() {
+        return de;
+    }
+
+    public void setDe(String de) {
+        this.de = de;
+    }
+
+    public String getAte() {
+        return ate;
+    }
+
+    public void setAte(String ate) {
+        this.ate = ate;
+    }
+
+    public EnumVo getTipoEntidade() {
+        return tipoEntidade;
+    }
+
+    public void setTipoEntidade(EnumVo tipoEntidade) {
+        this.tipoEntidade = tipoEntidade;
+    }
+}
