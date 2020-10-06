5dcd810299717504d838362eb283f0ee7612f918
josuepdc <jospdc@gmail.com>
2020-10-06 16:37:08 -0300

    Adicionando tipo de arquivo xlsx no mapeamento de repositorio. #63348

--- src/main/java/ipp/aci/boleia/dominio/servico/ArmazenamentoArquivosSd.java
+++ src/main/java/ipp/aci/boleia/dominio/servico/ArmazenamentoArquivosSd.java
@@ -60,6 +60,7 @@ public class ArmazenamentoArquivosSd {
         repositoriosPorTipoArquivo.put(TipoArquivo.JUSTIFICATIVA_NOTA, notaFiscalDados);
         repositoriosPorTipoArquivo.put(TipoArquivo.DOWNLOAD_PRESIGNED_NOTA_FISCAL_PDF, arquivoDados);
         repositoriosPorTipoArquivo.put(TipoArquivo.DOWNLOAD_PRESIGNED_NOTA_FISCAL_XML, arquivoDados);
+        repositoriosPorTipoArquivo.put(TipoArquivo.RELATORIO_48_HORAS_XLSX, arquivoDados);
     }
 
     /**
