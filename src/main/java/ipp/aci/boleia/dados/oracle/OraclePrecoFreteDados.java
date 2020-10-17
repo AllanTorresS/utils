91fcce96ffcd39a08962529f9fc5514e8f51ecea
fernando.bittencourt <fernando.bittencourt@radixeng.com.br>
2020-10-17 14:22:30 -0300

    Acertando ndd, #6548

--- src/main/java/ipp/aci/boleia/dados/oracle/OraclePrecoFreteDados.java
+++ src/main/java/ipp/aci/boleia/dados/oracle/OraclePrecoFreteDados.java
@@ -1,20 +1,8 @@
 package ipp.aci.boleia.dados.oracle;
 
-import ipp.aci.boleia.dados.IAgenciadorFreteTaxaDados;
 import ipp.aci.boleia.dados.IPrecoFreteDados;
-import ipp.aci.boleia.dominio.agenciadorfrete.AgenciadorFreteTaxa;
 import ipp.aci.boleia.dominio.agenciadorfrete.PrecoFrete;
-import ipp.aci.boleia.dominio.pesquisa.comum.InformacaoPaginacao;
-import ipp.aci.boleia.dominio.pesquisa.comum.ParametroPesquisa;
-import ipp.aci.boleia.dominio.pesquisa.comum.ResultadoPaginado;
-import ipp.aci.boleia.dominio.pesquisa.parametro.ParametroPesquisaDataMenorOuIgual;
-import ipp.aci.boleia.dominio.pesquisa.parametro.ParametroPesquisaIgual;
-import ipp.aci.boleia.util.UtilitarioCalculoData;
 import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 
 /**
