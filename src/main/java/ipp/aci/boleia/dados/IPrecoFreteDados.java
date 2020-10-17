ffa502ab7faf098ac2ab35e653a00472cd6069bb
fernando.bittencourt <fernando.bittencourt@radixeng.com.br>
2020-10-17 14:22:09 -0300

    Acertando ndd, #6548

---
+++ src/main/java/ipp/aci/boleia/dados/IPrecoFreteDados.java
@@ -0,0 +1,16 @@
+package ipp.aci.boleia.dados;
+
+
+import ipp.aci.boleia.dominio.Reembolso;
+import ipp.aci.boleia.dominio.agenciadorfrete.PrecoFrete;
+import ipp.aci.boleia.dominio.pesquisa.comum.ResultadoPaginado;
+import ipp.aci.boleia.dominio.vo.FiltroPesquisaReembolsoVo;
+
+import java.util.List;
+
+/**
+ * Contrato para implementacao de repositorios de entidades Reembolso
+ */
+public interface IPrecoFreteDados extends IRepositorioBoleiaDados<PrecoFrete> {
+
+}
