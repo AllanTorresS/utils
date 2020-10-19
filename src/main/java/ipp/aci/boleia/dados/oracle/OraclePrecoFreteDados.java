827a56ef0c541addba08875e9c231369adc42497
Laranjeiro <rafael.laranjeiro@radixeng.com.br>
2020-10-19 11:48:06 -0300

    #64599 - PRO-6548 -  [Visão Interna]  Filtro de status não está filtrando

--- src/main/java/ipp/aci/boleia/dados/oracle/OraclePrecoFreteDados.java
+++ src/main/java/ipp/aci/boleia/dados/oracle/OraclePrecoFreteDados.java
@@ -14,7 +14,6 @@ import ipp.aci.boleia.dominio.pesquisa.parametro.ParametroPesquisaNulo;
 import ipp.aci.boleia.dominio.vo.FiltroPesquisaPrecoFreteVo;
 import ipp.aci.boleia.util.Ordenacao;
 import ipp.aci.boleia.util.UtilitarioCalculoData;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -26,9 +25,6 @@ import java.util.List;
  */
 @Repository
 public class OraclePrecoFreteDados extends OracleRepositorioBoleiaDados<PrecoFrete> implements IPrecoFreteDados {
-
-    @Autowired
-    private UtilitarioCalculoData utilitarioCalculoData;
 
     /**
      * Instancia o repositorio
@@ -64,7 +60,7 @@ public class OraclePrecoFreteDados extends OracleRepositorioBoleiaDados<PrecoFre
 
         if (filtro.getDataVigencia() != null) {
             parametros.add(new ParametroPesquisaDataMenorOuIgual("dataVigencia",
-                    utilitarioCalculoData.obterUltimoInstanteDia(filtro.getDataVigencia())));
+                    UtilitarioCalculoData.obterUltimoInstanteDia(filtro.getDataVigencia())));
         }
 
         if (filtro.getStatus() != null && filtro.getStatus().getName() != null) {
