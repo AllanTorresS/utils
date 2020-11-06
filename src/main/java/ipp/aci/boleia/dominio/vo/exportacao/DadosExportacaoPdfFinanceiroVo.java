c0174e8057b6633eacdf1ff6b86929406a2974ce
josuepdc <jospdc@gmail.com>
2020-11-06 19:42:08 -0300

    Ajustando campos dos dados de exportação do financeiro em PDF, e removendo o TemplatePdf para utilizar centralizado no TemplatePlanilha. #61355

--- src/main/java/ipp/aci/boleia/dominio/vo/exportacao/DadosExportacaoPdfFinanceiroVo.java
+++ src/main/java/ipp/aci/boleia/dominio/vo/exportacao/DadosExportacaoPdfFinanceiroVo.java
@@ -1,18 +1,14 @@
 package ipp.aci.boleia.dominio.vo.exportacao;
 
-import java.util.List;
-
 /**
  * Vo com as informações utilizadas na exportação de PDF da tela do Financeiro.
  */
-public class DadosExportacaoPdfFinanceiroVo {
+public class DadosExportacaoPdfFinanceiroVo extends DadosExportacaoPdfBaseVo<RegistroExportacaoPdfCicloFinanceiroVo> {
     private String filtroPeriodo;
     private String filtroFrota;
     private String filtroPosto;
     private String filtroStatus;
     private String reembolsoTotalPago;
-    private Integer totalRegistros;
-    private List<CicloExportacaoPdfFinanceiroVo> ciclosExportacao;
 
     public String getFiltroPeriodo() {
         return filtroPeriodo;
@@ -52,21 +48,5 @@ public class DadosExportacaoPdfFinanceiroVo {
 
     public void setReembolsoTotalPago(String reembolsoTotalPago) {
         this.reembolsoTotalPago = reembolsoTotalPago;
-    }
-
-    public List<CicloExportacaoPdfFinanceiroVo> getCiclosExportacao() {
-        return ciclosExportacao;
-    }
-
-    public void setCiclosExportacao(List<CicloExportacaoPdfFinanceiroVo> ciclosExportacao) {
-        this.ciclosExportacao = ciclosExportacao;
-    }
-
-    public Integer getTotalRegistros() {
-        return totalRegistros;
-    }
-
-    public void setTotalRegistros(Integer totalRegistros) {
-        this.totalRegistros = totalRegistros;
     }
 }
