80086a3d77cf5cd12ebd12f0e25e9d20cbcd56ae
Lucas Rodrigues Teixeira Nunes <lucas.nunes@radixeng.com.br>
2020-12-03 18:00:59 -0300

    Iniciando implementação do filtro de unidade/empresa agregada, #69416

--- src/main/java/ipp/aci/boleia/dados/IEmpresaUnidadeDados.java
+++ src/main/java/ipp/aci/boleia/dados/IEmpresaUnidadeDados.java
@@ -2,8 +2,17 @@ package ipp.aci.boleia.dados;
 
 import ipp.aci.boleia.dominio.EmpresaUnidade;
 
+import java.util.List;
+
 /**
  * Repositório de dados da entidade Empresa/Unidade.
  */
 public interface IEmpresaUnidadeDados extends IRepositorioBoleiaDados<EmpresaUnidade> {
+
+    /**
+     * Obtém as empresas/unidades de uma frota
+     * @param idFrota O identificador da frota
+     * @return As empresas/unidades encontradas
+     */
+    List<EmpresaUnidade> obterPorFrota(Long idFrota);
 }
