957f4c4f4977caaedc545bc4dc84a2b37bb3299f
Tiago Marques <>
2021-01-28 11:22:14 -0300

    Adicionando JavaDoc #74809

--- src/main/java/ipp/aci/boleia/dados/IHistoricoFrotaParametroSistemaProdutoAbastecimentoDados.java
+++ src/main/java/ipp/aci/boleia/dados/IHistoricoFrotaParametroSistemaProdutoAbastecimentoDados.java
@@ -2,6 +2,10 @@ package ipp.aci.boleia.dados;
 
 import ipp.aci.boleia.dominio.HistoricoFrotaParametroSistemaProdutoAbastecimento;
 
-
+/**
+ * Repositório de dados da classe {@link HistoricoFrotaParametroSistemaProdutoAbastecimento}.
+ *
+ * @author tiago.marques
+ */
 public interface IHistoricoFrotaParametroSistemaProdutoAbastecimentoDados extends IRepositorioBoleiaDados<HistoricoFrotaParametroSistemaProdutoAbastecimento> {
 }
