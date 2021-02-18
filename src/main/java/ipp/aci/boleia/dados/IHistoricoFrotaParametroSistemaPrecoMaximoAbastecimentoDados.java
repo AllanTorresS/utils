52f7a3ffce90819d1b870bcdb5964fe0652d0a66
Allan Torres dos Santos <allan.santos@radixeng.com.br>
2021-02-18 09:21:50 -0300

    Feature AZR-76628 - Tela de detalhamento de Parâmetro de Limite permitido para Produtos e Serviços #76628

---
+++ src/main/java/ipp/aci/boleia/dados/IHistoricoFrotaParametroSistemaPrecoMaximoAbastecimentoDados.java
@@ -0,0 +1,11 @@
+package ipp.aci.boleia.dados;
+
+import ipp.aci.boleia.dominio.HistoricoFrotaParametroSistemaPrecoMaximoAbastecimento;
+
+/**
+ * Repositório de dados da classe {@link HistoricoFrotaParametroSistemaPrecoMaximoAbastecimento}.
+ *
+ * @author allan.santos
+ */
+public interface IHistoricoFrotaParametroSistemaPrecoMaximoAbastecimentoDados extends IRepositorioBoleiaDados<HistoricoFrotaParametroSistemaPrecoMaximoAbastecimento> {
+}
