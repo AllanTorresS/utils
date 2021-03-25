78f30432b406ac9a69bceed58a7151e52149abb2
joseph.sousa <joseph.sousa@radixeng.com.br>
2021-03-25 15:25:28 -0300

    implementando exclusao logica para a entidade tag conectcar

--- src/main/java/ipp/aci/boleia/dominio/TagConectcar.java
+++ src/main/java/ipp/aci/boleia/dominio/TagConectcar.java
@@ -1,23 +1,15 @@
 package ipp.aci.boleia.dominio;
 
-import java.util.Date;
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
-
+import ipp.aci.boleia.dominio.interfaces.IExclusaoLogicaComData;
+import ipp.aci.boleia.dominio.interfaces.IPersistente;
 import org.hibernate.annotations.Where;
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.NotAudited;
 
-import ipp.aci.boleia.dominio.interfaces.IPersistente;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Representa a tabela de AtivacaoTag
@@ -25,7 +17,7 @@ import ipp.aci.boleia.dominio.interfaces.IPersistente;
 @Entity
 @Audited
 @Table(name = "TAG_CONECTCAR")
-public class TagConectcar implements IPersistente {
+public class TagConectcar implements IPersistente, IExclusaoLogicaComData{
 
 	private static final long serialVersionUID = -8043835722308868542L;
 
@@ -109,6 +101,11 @@ public class TagConectcar implements IPersistente {
 		this.dataBloqueio = dataBloqueio;
 	}
 
+	@Override
+	public Boolean getExcluido() {
+		return this.dataExclusao != null;
+	}
+
 	public Date getDataExclusao() {
 		return dataExclusao;
 	}
@@ -126,19 +123,11 @@ public class TagConectcar implements IPersistente {
 	}
 
 	public boolean isAtivo(){
-		if(dataBloqueio == null) {
-			return true;
-		}
-		
-		return false;
+		return dataBloqueio == null;
 	}
 	
 	public boolean isBloqueado(){
-		if(dataBloqueio != null) {
-			return true;
-		}
-		
-		return false;
+		return dataBloqueio != null;
 	}
 	
 	public Integer getStatus() {
@@ -169,11 +158,10 @@ public class TagConectcar implements IPersistente {
 			return false;
 		TagConectcar other = (TagConectcar) obj;
 		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+			return other.id == null;
+		} else {
+			return id.equals(other.id);
+		}
 	}
 
 	public Veiculo getVeiculoMaisRecente() {
