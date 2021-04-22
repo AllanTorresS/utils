dfd27295557a8ba1ec2c8fbe20fbf981b3d41021
Josué Pereira da Conceição <josue.conceicao@radixeng.com.br>
2021-04-22 13:41:26 -0300

    Ajustando javadoc com parâmetro faltante. #81881

--- src/main/java/ipp/aci/boleia/dominio/vo/FiltroPesquisaLocalizacaoVo.java
+++ src/main/java/ipp/aci/boleia/dominio/vo/FiltroPesquisaLocalizacaoVo.java
@@ -27,6 +27,7 @@ public class FiltroPesquisaLocalizacaoVo {
 	 * Pesquisa a localiacao geografica de um ponto
 	 * @param ponto O ponto de referencia
 	 * @param distancia A distancia
+	 * @param perfilPontoDeVenda O perfil do ponto de venda
 	 */
 	public FiltroPesquisaLocalizacaoVo(CoordenadaGeograficaVo ponto, double distancia, PerfilPontoDeVenda perfilPontoDeVenda) {
 		double distanciaLatitude = UtilitarioCoordenadasGeograficas.converterDistanciaLatitude(distancia);
@@ -42,6 +43,7 @@ public class FiltroPesquisaLocalizacaoVo {
 	 * Pesquisa a localizacao geografica de uma rota, dado um grupo de pontos
 	 * @param filtrosCoordenadas Os filtros de coordenadas
 	 * @param margemGrausFiltroCoordenadas A margem em graus do filtro de coordenadas
+	 * @param perfilPontoDeVenda O perfil do ponto de venda
 	 */
 	public FiltroPesquisaLocalizacaoVo(List<List<CoordenadaVo>> filtrosCoordenadas, BigDecimal margemGrausFiltroCoordenadas, PerfilPontoDeVenda perfilPontoDeVenda) {
 		this.filtrosCoordenadas = filtrosCoordenadas;
