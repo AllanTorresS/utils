baa733a88178dc93b4bb768d051bac056cf1e0b6
pedro.silva <pedro.silva@radixeng.com.br>
2021-06-15 13:29:46 -0300

    Incluindo verificação de sinal do valor que está sendo passado para a realização do débito na conta do beneficiario, #87660

--- src/main/java/ipp/aci/boleia/dominio/servico/beneficios/OperacaoContaBeneficiarioSd.java
+++ src/main/java/ipp/aci/boleia/dominio/servico/beneficios/OperacaoContaBeneficiarioSd.java
@@ -4,6 +4,8 @@ import ipp.aci.boleia.dados.IContaBeneficiarioDados;
 import ipp.aci.boleia.dados.IOperacaoContaBeneficiarioDados;
 import ipp.aci.boleia.dominio.beneficios.ContaBeneficiario;
 import ipp.aci.boleia.dominio.beneficios.OperacaoContaBeneficiario;
+import ipp.aci.boleia.util.excecao.Erro;
+import ipp.aci.boleia.util.excecao.ExcecaoValidacao;
 import ipp.aci.boleia.util.negocio.UtilitarioAmbiente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -26,13 +28,19 @@ public class OperacaoContaBeneficiarioSd {
     private UtilitarioAmbiente utilitarioAmbiente;
 
     /**
-     * Cria uma operação de uma conta de beneficiário.
+     * Cria uma operação de débito em uma conta de beneficiário.
      *
      * @param contaBeneficiario a conta que sofreu a operação.
      * @param valorOperacao o valor da operação.
      * @return operação criada.
+     *
+     * @throws ExcecaoValidacao lançada caso seja passado um valor menor ou igual a zero para a criação do débito.
      */
-    public OperacaoContaBeneficiario criarOperacaoConta(ContaBeneficiario contaBeneficiario, BigDecimal valorOperacao) {
+    public OperacaoContaBeneficiario criarOperacaoDebitoConta(ContaBeneficiario contaBeneficiario, BigDecimal valorOperacao) throws ExcecaoValidacao {
+        if(valorOperacao.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new ExcecaoValidacao(Erro.ERRO_GENERICO);
+        }
+
         OperacaoContaBeneficiario operacao = new OperacaoContaBeneficiario();
         operacao.setContaBeneficiario(contaBeneficiario);
         operacao.setValorTotal(valorOperacao);
