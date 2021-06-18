6c36ad837f39ddcaab8b66d81c5682b42769a823
paulo.mendes <paulo.mendes@radixeng.com.br>
2021-06-18 10:08:08 -0300

    Ajuste imports

--- src/main/java/ipp/aci/boleia/dados/oracle/OracleTagConectcarDados.java
+++ src/main/java/ipp/aci/boleia/dados/oracle/OracleTagConectcarDados.java
@@ -1,5 +1,14 @@
 package ipp.aci.boleia.dados.oracle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Repository;
+
 import ipp.aci.boleia.dados.ITagConectcarDados;
 import ipp.aci.boleia.dominio.TagConectcar;
 import ipp.aci.boleia.dominio.enums.StatusAtivacao;
@@ -10,13 +19,6 @@ import ipp.aci.boleia.dominio.pesquisa.parametro.ParametroPesquisaIgual;
 import ipp.aci.boleia.dominio.pesquisa.parametro.ParametroPesquisaIgualIgnoreCase;
 import ipp.aci.boleia.dominio.pesquisa.parametro.ParametroPesquisaNulo;
 import ipp.aci.boleia.dominio.vo.FiltroPesquisaTagConectcarVo;
-import org.apache.commons.collections4.CollectionUtils;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.NoResultException;
-import javax.persistence.Query;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Repositório de entidades TagConectcar
