eaa0af767f03b4df2a7b4c461dbd59d16eb43303
luis.nogueira <luis.nogueira@radixeng.com.br>
2021-06-30 14:10:09 -0300

    implementação dos serviços de aprovação e cancelamento de proposta

---
+++ src/main/java/ipp/aci/boleia/dados/IPropostaAntecipacaoDados.java
@@ -0,0 +1,11 @@
+package ipp.aci.boleia.dados;
+
+import ipp.aci.boleia.dominio.PropostaAntecipacao;
+
+/**
+ * Contrato de implementação de repositório de Proposta Amtecipacao
+ */
+public interface IPropostaAntecipacaoDados extends IRepositorioBoleiaDados<PropostaAntecipacao>{
+
+    PropostaAntecipacao obterPorIdParceiro(String idProposta);
+}
