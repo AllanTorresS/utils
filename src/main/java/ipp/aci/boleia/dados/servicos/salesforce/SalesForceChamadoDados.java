e696c425ea18bf9f2cf2207f813e21835154dda6
Luiz Miguel Viana Barbosa <43626451+LuizMVB@users.noreply.github.com>
2021-07-05 18:42:04 -0300

    Ajustes finais #89679

--- src/main/java/ipp/aci/boleia/dados/servicos/salesforce/SalesForceChamadoDados.java
+++ src/main/java/ipp/aci/boleia/dados/servicos/salesforce/SalesForceChamadoDados.java
@@ -267,7 +267,7 @@ public class SalesForceChamadoDados extends AcessoSalesForceBase implements ICha
             if(this.statusCode == HttpStatus.OK.value()) {
                 PicklistVo picklist = UtilitarioJson.toObjectWithConfigureFailOnUnknowProperties(this.responseBody.toString(), PicklistVo.class, false);
                 return picklist.getValores().stream()
-                        .filter(p -> p.getValor().equals(ConstantesSalesForce.CAMPO_MOTIVO_CANCELAMENTO_VALOR_DUPLICADO) == false)
+                        .filter(p -> !p.getValor().equals(ConstantesSalesForce.CAMPO_MOTIVO_CANCELAMENTO_VALOR_DUPLICADO))
                         .collect(Collectors.toList());
             }
             return new ArrayList<>();
@@ -281,15 +281,11 @@ public class SalesForceChamadoDados extends AcessoSalesForceBase implements ICha
         vo.setMotivoCancelamento(motivoCancelamento);
         vo.setDescricaoCancelamento(descricaoCancelamento);
 
-        String urlCancelamentoComIdSalesforce = formatarQueryParaCancelamento(urlCancelamento, idSalesforce);
+        String urlCancelamentoComIdSalesforce = format(urlCancelamento, idSalesforce);
 
         prepararRequisicao(urlCancelamentoComIdSalesforce, vo);
 
         enviarRequisicaoPatch(this::trataRespostaCancelamento);
-    }
-
-    private String formatarQueryParaCancelamento(String urlCancelamento, String idSalesforce) {
-        return format(urlCancelamento, idSalesforce);
     }
 
     /**
