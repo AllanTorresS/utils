61ef45bf2409a5899523b45d97bfbdae2dfd18fa
lucas.santiago <lucas.santiago@radixeng.com.br>
2021-07-12 15:26:20 -0300

    Criação Classes relacionadas a ContaBeneficio para a configuração de benefícios, #89685

---
+++ src/main/java/ipp/aci/boleia/dados/IContaBeneficioDados.java
@@ -0,0 +1,10 @@
+package ipp.aci.boleia.dados;
+
+import ipp.aci.boleia.dominio.beneficios.ContaBeneficio;
+
+/**
+ * Contrato para implementação de repositórios de entidades ContaBeneficio
+ */
+public interface IContaBeneficioDados extends IRepositorioBoleiaDados<ContaBeneficio> {
+
+}
