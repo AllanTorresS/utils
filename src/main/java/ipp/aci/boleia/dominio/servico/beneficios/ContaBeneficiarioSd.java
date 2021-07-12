8a6f826904986a74111347bcd2206e05b0e8f044
pedro.silva <pedro.silva@radixeng.com.br>
2021-07-12 19:40:33 -0300

    Criação e configuração inicial da conta do beneficiário sendo feira na criação do beneficiário, #89685

--- src/main/java/ipp/aci/boleia/dominio/servico/beneficios/ContaBeneficiarioSd.java
+++ src/main/java/ipp/aci/boleia/dominio/servico/beneficios/ContaBeneficiarioSd.java
@@ -1,14 +1,20 @@
 package ipp.aci.boleia.dominio.servico.beneficios;
 
 import ipp.aci.boleia.dados.IBeneficiarioDados;
+import ipp.aci.boleia.dados.IBeneficioDados;
 import ipp.aci.boleia.dados.IContaBeneficiarioDados;
+import ipp.aci.boleia.dados.IContaBeneficioDados;
 import ipp.aci.boleia.dominio.beneficios.Beneficiario;
+import ipp.aci.boleia.dominio.beneficios.Beneficio;
 import ipp.aci.boleia.dominio.beneficios.ContaBeneficiario;
+import ipp.aci.boleia.dominio.beneficios.ContaBeneficio;
 import ipp.aci.boleia.util.negocio.UtilitarioAmbiente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Serviços de domínio da entidade {@link ContaBeneficiario}.
@@ -20,8 +26,16 @@ public class ContaBeneficiarioSd {
 
     @Autowired
     private IBeneficiarioDados beneficiarioDados;
+
     @Autowired
     private IContaBeneficiarioDados contaBeneficiarioDados;
+
+    @Autowired
+    private IContaBeneficioDados contaBeneficioDados;
+
+    @Autowired
+    private IBeneficioDados beneficioDados;
+
     @Autowired
     private UtilitarioAmbiente utilitarioAmbiente;
 
@@ -38,6 +52,29 @@ public class ContaBeneficiarioSd {
         contaBeneficiario.setSaldo(BigDecimal.ZERO);
         contaBeneficiario.setDataCriacao(utilitarioAmbiente.buscarDataAmbiente());
         contaBeneficiario.setDataAtualizacao(utilitarioAmbiente.buscarDataAmbiente());
-        return contaBeneficiarioDados.armazenar(contaBeneficiario);
+        contaBeneficiario = contaBeneficiarioDados.armazenar(contaBeneficiario);
+        contaBeneficiario.setContasBeneficio(configurarBeneficiosIniciais(contaBeneficiario));
+        return contaBeneficiario;
+    }
+
+    /**
+     * Realiza a configuração padrão de benefícios para uma {@link ContaBeneficiario}.
+     *
+     * @param contaBeneficiario Conta que será configurada.
+     * @return a lista com os vinculos entre {@link ContaBeneficiario} e {@link Beneficio}.
+     */
+    private List<ContaBeneficio> configurarBeneficiosIniciais(ContaBeneficiario contaBeneficiario) {
+        List<Beneficio> beneficios = beneficioDados.obterTodos(null);
+        List<ContaBeneficio> listaContaBeneficio = new ArrayList<>();
+        beneficios.forEach(beneficio -> {
+            ContaBeneficio contaBeneficio = new ContaBeneficio();
+            contaBeneficio.setBeneficio(beneficio);
+            contaBeneficio.setBeneficioConfigurado(true);
+            contaBeneficio.setContaBeneficiario(contaBeneficiario);
+            contaBeneficio.setDataCriacao(utilitarioAmbiente.buscarDataAmbiente());
+            contaBeneficio.setDataAtualizacao(utilitarioAmbiente.buscarDataAmbiente());
+            listaContaBeneficio.add(contaBeneficio);
+        });
+        return contaBeneficioDados.armazenarLista(listaContaBeneficio);
     }
 }
