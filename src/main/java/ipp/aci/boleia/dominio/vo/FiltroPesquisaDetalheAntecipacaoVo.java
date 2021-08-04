f2eba3e09c087d0f941fe3cff92437f75748357f
Tiago Marques <tiago.marques@radixeng.com.br>
2021-08-04 01:11:31 -0300

    Exportação Detalhe Antecipação #91948

---
+++ src/main/java/ipp/aci/boleia/dominio/vo/FiltroPesquisaDetalheAntecipacaoVo.java
@@ -0,0 +1,75 @@
+package ipp.aci.boleia.dominio.vo;
+
+import ipp.aci.boleia.dominio.pesquisa.comum.BaseFiltroPaginado;
+
+import java.util.Date;
+
+/**
+ * Filtro de pesquisa de antecipações
+ */
+public class FiltroPesquisaDetalheAntecipacaoVo extends BaseFiltroPaginado {
+
+    private Long id;
+    private String nomePosto;
+    private String cnpjPosto;
+    private String valorSolicitado;
+    private String dataPagamento;
+    private EnumVo status;
+    private Date dataSolicitacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomePosto() {
+        return nomePosto;
+    }
+
+    public void setNomePosto(String nomePosto) {
+        this.nomePosto = nomePosto;
+    }
+
+    public String getCnpjPosto() {
+        return cnpjPosto;
+    }
+
+    public void setCnpjPosto(String cnpjPosto) {
+        this.cnpjPosto = cnpjPosto;
+    }
+
+    public String getValorSolicitado() {
+        return valorSolicitado;
+    }
+
+    public void setValorSolicitado(String valorSolicitado) {
+        this.valorSolicitado = valorSolicitado;
+    }
+
+    public String getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(String dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public EnumVo getStatus() {
+        return status;
+    }
+
+    public void setStatus(EnumVo status) {
+        this.status = status;
+    }
+
+    public Date getDataSolicitacao() {
+        return dataSolicitacao;
+    }
+
+    public void setDataSolicitacao(Date dataSolicitacao) {
+        this.dataSolicitacao = dataSolicitacao;
+    }
+}
