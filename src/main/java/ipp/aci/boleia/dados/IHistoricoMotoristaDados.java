ea3fc5a73ae41a6a71e7a1f6ae2acaecc8939d3e
josuepdc <jospdc@gmail.com>
2021-08-06 10:39:20 -0300

    Criando consulta de históricos de motorista por id do motorista. #90845

--- src/main/java/ipp/aci/boleia/dados/IHistoricoMotoristaDados.java
+++ src/main/java/ipp/aci/boleia/dados/IHistoricoMotoristaDados.java
@@ -1,8 +1,10 @@
 package ipp.aci.boleia.dados;
 
 import ipp.aci.boleia.dominio.HistoricoMotorista;
+import ipp.aci.boleia.dominio.Motorista;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Contrato para implementacao de repositorios da entidade {@link HistoricoMotorista}.
@@ -19,5 +21,13 @@ public interface IHistoricoMotoristaDados extends IRepositorioBoleiaDados<Histor
      * @return O historico encontrado.
      */
     HistoricoMotorista obterHistoricoMotoristaPorData(Long cdMotorista, Date data);
+
+    /**
+     * Obtem a lista de historicos de alterações de um derterminado motorista.
+     *
+     * @param motorista Entidade do {@link ipp.aci.boleia.dominio.Motorista} atrelado ao historico.
+     * @return A lista de historicos encontrados.
+     */
+    List<HistoricoMotorista> obterTodosPorMotorista(Motorista motorista);
     
 }
