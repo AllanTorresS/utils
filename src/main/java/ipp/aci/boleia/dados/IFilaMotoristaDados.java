2da3ff3240d79960b6e13355122580dec2868a6b
josuepdc <jospdc@gmail.com>
2021-08-09 12:15:18 -0300

    Corrigindo javadoc. #90845

--- src/main/java/ipp/aci/boleia/dados/IFilaMotoristaDados.java
+++ src/main/java/ipp/aci/boleia/dados/IFilaMotoristaDados.java
@@ -28,6 +28,9 @@ public interface IFilaMotoristaDados {
      * Envia o {@link Motorista} para a fila de anonimização e exclusão de dados da auditoria
      *
      * @param motorista O motorista a ter os dados anonimizados e exlcuidos da auditoria
+     * @param motoristaAnonimo O motorista anônimo a anonimizar os dados do motorista
+     * @param usuarioMotorista O usuario motorista a ter os dados anonimizados e exlcuidos da auditoria
+     * @param usuario O usuario a ter os dados anonimizados e exlcuidos da auditoria
      */
     void enviarMotoristaParaFilaDeAnonimizacaoExclusaoAuditoria(Motorista motorista, Motorista motoristaAnonimo, UsuarioMotorista usuarioMotorista, Usuario usuario);
 }
