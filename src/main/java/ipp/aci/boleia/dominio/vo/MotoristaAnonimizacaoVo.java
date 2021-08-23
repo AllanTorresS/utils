cc82cca7e6a1681756f96e1e2c2a2849c04b6d30
Erick Guimarães de Oliveira <erick.oliveira@radixeng.com.br>
2021-08-23 15:30:07 -0300

    Corrigindo import de *

--- src/main/java/ipp/aci/boleia/dominio/vo/MotoristaAnonimizacaoVo.java
+++ src/main/java/ipp/aci/boleia/dominio/vo/MotoristaAnonimizacaoVo.java
@@ -1,6 +1,11 @@
 package ipp.aci.boleia.dominio.vo;
 
-import ipp.aci.boleia.dominio.*;
+import ipp.aci.boleia.dominio.AutorizacaoPagamento;
+import ipp.aci.boleia.dominio.AutorizacaoPagamentoEdicao;
+import ipp.aci.boleia.dominio.DispositivoMotorista;
+import ipp.aci.boleia.dominio.DispositivoMotoristaPedido;
+import ipp.aci.boleia.dominio.FluxoAbastecimentoMotoristaConfig;
+import ipp.aci.boleia.dominio.Motorista;
 
 import java.util.Date;
 import java.util.List;
