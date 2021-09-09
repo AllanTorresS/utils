674f82068c478baf73d8856fb5a48553bd2ad2f2
Allan Torres dos Santos <allan.santos@radixeng.com.br>
2021-09-09 12:48:08 -0300

    correção de bugs [PRO-7442] Dados do filtro não estão sendo resetados ao alterar entre abas, [PRO-7442] Erro interno ao ordenar coluna meio de pagamento e [PRO-7441] Valor da compra de créditos está incorreto #96105 #96106 #96104

--- src/main/java/ipp/aci/boleia/dominio/vo/MundipaggWebhookChargePedidoVo.java
+++ src/main/java/ipp/aci/boleia/dominio/vo/MundipaggWebhookChargePedidoVo.java
@@ -2,6 +2,8 @@ package ipp.aci.boleia.dominio.vo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.math.BigDecimal;
+
 /**
  * Classe com informacoes relacionadas sobre o meio de pagamento do pedido de credito enviado pelo webhook da Mundipagg
  */
@@ -9,6 +11,11 @@ public class MundipaggWebhookChargePedidoVo {
 
     @JsonProperty("payment_method")
     private String meioDePagamento;;
+
+    @JsonProperty("paid_amount")
+    private BigDecimal valorPago;
+
+    private String status;
 
 
     public MundipaggWebhookChargePedidoVo() {
@@ -20,6 +27,22 @@ public class MundipaggWebhookChargePedidoVo {
 
     public void setMeioDePagamento(String meioDePagamento) {
         this.meioDePagamento = meioDePagamento;
+    }
+
+    public BigDecimal getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(BigDecimal valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
  
