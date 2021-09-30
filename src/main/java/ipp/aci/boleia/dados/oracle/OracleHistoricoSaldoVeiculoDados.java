20e69d831e7dca0a4e432da04bed4c81b7ba49d7
Daniel Brasil <daniel.brasil@radixeng.com.br>
2021-09-30 13:55:31 -0300

    Consulta de histórico de saldo veículo #PRO-7576

--- src/main/java/ipp/aci/boleia/dados/oracle/OracleHistoricoSaldoVeiculoDados.java
+++ src/main/java/ipp/aci/boleia/dados/oracle/OracleHistoricoSaldoVeiculoDados.java
@@ -3,7 +3,15 @@ package ipp.aci.boleia.dados.oracle;
 import ipp.aci.boleia.dados.IHistoricoSaldoVeiculosDados;
 import ipp.aci.boleia.dominio.HistoricoFrotaParametroSistemaCota;
 import ipp.aci.boleia.dominio.HistoricoSaldoVeiculo;
+import ipp.aci.boleia.dominio.enums.StatusAtivacao;
+import ipp.aci.boleia.dominio.pesquisa.comum.ParametroOrdenacaoColuna;
+import ipp.aci.boleia.dominio.pesquisa.comum.ParametroPesquisa;
+import ipp.aci.boleia.dominio.pesquisa.parametro.ParametroPesquisaIgual;
+import ipp.aci.boleia.util.Ordenacao;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Implementação do repositório de dados da entidade {@link HistoricoFrotaParametroSistemaCota}.
@@ -18,5 +26,15 @@ public class OracleHistoricoSaldoVeiculoDados extends OracleRepositorioBoleiaDad
      */
     public OracleHistoricoSaldoVeiculoDados() {
         super(HistoricoSaldoVeiculo.class);
+    }
+
+    @Override
+    public List<HistoricoSaldoVeiculo> obterPorIdVeiculo(Long idVeiculo) {
+        List<ParametroPesquisa> parametros = new ArrayList<>();
+        parametros.add(new ParametroPesquisaIgual("veiculo.id", idVeiculo));
+        parametros.add(new ParametroPesquisaIgual("veiculo.status", StatusAtivacao.ATIVO.getValue()));
+        parametros.add(new ParametroPesquisaIgual("veiculo.excluido", Boolean.FALSE));
+
+        return pesquisar(new ParametroOrdenacaoColuna("dataAlteracao", Ordenacao.DECRESCENTE), parametros.toArray(new ParametroPesquisa[parametros.size()]));
     }
 }
