e1c27d8681ec39b5799d4b9ad1933598efce0713
josuepdc <jospdc@gmail.com>
2021-10-15 20:11:11 -0300

    Adicionando ignore properties no Vo do allowme

--- src/main/java/ipp/aci/boleia/dominio/vo/DispositivoAllowMeVo.java
+++ src/main/java/ipp/aci/boleia/dominio/vo/DispositivoAllowMeVo.java
@@ -1,5 +1,6 @@
 package ipp.aci.boleia.dominio.vo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.List;
 /**
  * Classe com informacoes relacionadas ao dispositivo Allow Me
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DispositivoAllowMeVo {
 
     private String id;
